b0ad3b54fab17ded0eb5b341d1c952fdde0b37b5
Cameron Gulley <Speakender3@gmail.com>
2019-02-05 10:06:48 -0800

    adding moar files

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+name1 = 'Tom'
+name2 = 'Dick'
+name3 = 'Harry'
+name4 = input("Gimme name:")
+
+print("{:>30}".format(name1))
+print("{:>30}".format(name2))
+print("{:>30}".format(name3))
+print("{:>30}".format(name4))
